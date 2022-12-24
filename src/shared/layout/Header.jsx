ba7b7e1890d87e477280499d5b2f0aad785a0ced
pablaw <chulhwa7@gmail.com>
2022-12-24 17:51:02 +0900

    style: 헤더 로고 사이즈 수정

--- src/shared/layout/Header.jsx
+++ src/shared/layout/Header.jsx
@@ -17,26 +17,28 @@ const Header = () => {
   };
 
   return (
-    <div className={classes.header}>
+    <div className={classes.headerContainer}>
       <Link to={"/"} className={classes.logo_btn}>
-        <img src={logo} />
+        <img style={{ objectFit: "cover" }} src={logo} />
       </Link>
-      <div className={classes.right_btn}>
-        <p>당신의 공간을 항해비앤비하세요</p>
-        <div className={classes.icons}>
-          {btnmodal ? <BtnModal onModal={onModal} /> : null}
-          <button
-            className={classes.icons_login}
-            onClick={onClickBtnModalHandler}
-          >
-            <FaBars size="17px" color="#524f4f" />
-          </button>
-
-          <Link to={"/mypage"}>
-            <button className={classes.icons_my_btn}>
-              <FaUserAlt size="18px" color="#524f4f" />
+      <div className={classes.headerButtonGroup}>
+        <div className={classes.right_btn}>
+          <p>당신의 공간을 항해비앤비하세요</p>
+          <div className={classes.icons}>
+            {btnmodal ? <BtnModal onModal={onModal} /> : null}
+            <button
+              className={classes.icons_login}
+              onClick={onClickBtnModalHandler}
+            >
+              <FaBars size="17px" color="#524f4f" />
             </button>
-          </Link>
+
+            <Link to={"/mypage"}>
+              <button className={classes.icons_my_btn}>
+                <FaUserAlt size="18px" color="#524f4f" />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
